5e84b9129640aed399e72721e0f5fd5dc5d494ee
Dan M McGinnis <mcginnis.dan@gmail.com>
2014-04-29 15:00:47 -0400

    updated splash screen

--- src/com/cis4350/game/SplashLoadingScreen.java
+++ src/com/cis4350/game/SplashLoadingScreen.java
@@ -13,7 +13,7 @@ public class SplashLoadingScreen extends Screen {
     @Override
     public void update(float deltaTime) {
         Graphics g = game.getGraphics();
-        Assets.splash= g.newImage("splash.jpg", ImageFormat.RGB565);
+        Assets.splash= g.newImage("newSplash.jpg", ImageFormat.RGB565);
 
         
         game.setScreen(new LoadingScreen(game));
